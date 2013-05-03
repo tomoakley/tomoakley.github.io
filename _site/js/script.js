73b6a93a64725f6e2f92f5453eaf127b76f0876f
Tom Oakley <tom.oakley@me.com>
2013-05-03 21:24:33 +0100

    starting from a clean state

--- _site/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$(".contact-me").click(function(event) {
-		event.preventDefault ? event.preventDefault() : event.returnValue = false;
-		$(this).toggleClass("active");
-		$(".contact-details").slideToggle("fast");
-	});
-});
